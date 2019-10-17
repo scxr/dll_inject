65479409f2aaab0600da628eb0e5f5a532cf361b
cswil <56652842+cswil@users.noreply.github.com>
2019-10-17 18:27:32 +0100

    Create get_dll&pid.py
    get the pid of the dll spawned

---
+++ get_dll&pid.py
@@ -0,0 +1,12 @@
+import psutil,os
+got=False
+for proc in psutil.process_iter():
+    try:
+        p=psutil.Process(os.getpid())
+        for dll in p.memory_maps():
+            if got == False: # stop an infinite loop, we only need 1 dll
+                print(os.getpid(),dll.path)
+            else:
+                break
+    except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+        pass
