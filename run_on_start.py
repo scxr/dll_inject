83b139a53e88cb8ca224bdd7fd0e146e0dad54b6
cswil <56652842+cswil@users.noreply.github.com>
2019-10-17 18:25:41 +0100

    Create run_on_start.py
    add my injector to startup

---
+++ run_on_start.py
@@ -0,0 +1,9 @@
+"""
+A script that adds the program to startup
+"""
+import os
+fp=os.getcwd()+"/main.py"
+os.chdir("C:/Users/"+os.path.expandvars("%userprofile%").split("\\")[2]+"/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup")
+with open("start.bat","w") as f:
+    f.write('start '+fp)
+    f.close()
